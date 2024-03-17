3f1264cf959b499cd486b31ef7a16bb6bd6c726a
Alexander Medvedev <razerguy999@gmail.com>
2024-03-17 22:07:38 +0200

    Commit HW-14 Extended OrderTest class by another 2 tests Added Class OrderDto for .build pattern RandomDataGenerator class to generate order fields Gson to serialize request from Java object to Json format Lombok dependency

--- src/test/java/delivery/api/OrderTest.java
+++ src/test/java/delivery/api/OrderTest.java
@@ -3,6 +3,7 @@ package delivery.api;
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import delivery.utils.ApiClient;
 
@@ -16,6 +17,24 @@ public class OrderTest extends BaseSetupApi {
         Assertions.assertAll("Test description",
                 () -> Assertions.assertEquals(HttpStatus.SC_OK, response.getStatusCode(), "Status code is OK")
         );
+    }
+    @RepeatedTest(5)
+    void createRandomOrderAndCheckResponse() {
 
+        Response response = ApiClient.createRandomOrder(getAuthenticatedRequestSpecification());
+
+        Assertions.assertAll("Test description",
+                () -> Assertions.assertEquals(HttpStatus.SC_OK, response.getStatusCode(), "Status code is OK")
+        );
+    }
+    @RepeatedTest(5)
+    void createRandomOrderStatusAndCheckResponse() {
+
+        Response response = ApiClient.createRandomOrderStatus(getAuthenticatedRequestSpecification());
+
+        Assertions.assertAll("Test description",
+                () -> Assertions.assertEquals(HttpStatus.SC_OK, response.getStatusCode(), "Status code is OK")
+        );
     }
 }
+
